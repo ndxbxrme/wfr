8a565b9cf7cb45f8f18aa7b5b210f8d8b2952759
ndxbxrme <lewis_the_cat@hotmail.com>
2021-03-17 11:39:30 +0000

    payroll update

--- app/components/getPayroll.js
+++ app/components/getPayroll.js
@@ -4,13 +4,16 @@ module.exports = (subdomain, dateFrom, dateTo, debug) => {
   return new Promise(async (resolve, reject) => {
     let payslips = [];
     let date = dateFrom;
-    while(date <= dateTo) {
-      const {periods} = await api.fetch('get', 'payroll/' + date.getFullYear(), subdomain, null, null, debug);
+    let year = dateFrom.getFullYear();
+    const endYear = dateTo.getFullYear();
+    while(year <= endYear) {
+      console.log('getting payroll', date);
+      const {periods} = await api.fetch('get', 'payroll/' + year, subdomain, null, null, debug);
       for(let f=0; f<periods.length; f++) {
         const {period} = (await api.fetch('get', periods[f].url.replace(api.faUri, ''), subdomain));
         payslips = [...payslips, ...period.payslips];
       }
-      date = new Date(date.getFullYear() + 1, date.getMonth(), date.getDate());
+      year++;
     }
     resolve(payslips);
   });
